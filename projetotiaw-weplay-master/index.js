1fbf85c7e8bda44a98403f8bae2ec674be082424
Gabriel Carlos Freitas Lorenzin <51822398+hvvvpl@users.noreply.github.com>
2023-05-10 15:29:27 -0300

    commit inicial

---
+++ projetotiaw-weplay-master/index.js
@@ -0,0 +1,67 @@
+let lks = document.getElementById('lks')
+if (localStorage.getItem("token") == null) {
+    alert("É necessário estar logado para continuar")
+    window.location.href = "login.html"
+}
+else {
+    lks.innerHTML = '<div id= "lks"class="links">   <a href="#destaque">Destaques</a> <a href="#Lançamentos">Games</a> <a href="#plataformas">Plataformas</a> <a href="#Publishers">Notícias</a> <a href="perfil.html">Perfil</a><button class="btnsair"id="sair" onclick="sair()"><strong>Sair</button></strong></div>'
+}
+function sair() {
+    window.location.href = "login.html"
+}
+
+fetch('https://api.rawg.io/api/games?key=6755f0d58e914b038cbf17ec78d014b2&platforms=18,1,7')
+    .then((res) => res.json())
+    .then(data => {
+        let str = ''
+        for (let i = 0; i < data.results.length; i++) {
+            let jogo = data.results[i]
+            str += `<div class="card">
+                 <img src="${jogo.background_image}" class="card-img-top" alt="...">
+                 <div class="card-body">
+                     <h5 class="card-title"<b>${jogo.name}</b></h5>
+                     <p class="card-text">
+                     ${jogo.released}
+                     </p>
+                     <a href="https://rawg.io/games/${jogo.id}" target="_blank" class="btn btn-primary">Mais detalhes</a>
+                 </div>
+                 </div>`
+        }
+        document.getElementById('tela').innerHTML = str
+    })
+// RETURN SEARCH
+
+const apiKey = '6755f0d58e914b038cbf17ec78d014b2'
+let txtSearch = document.getElementById('txtSearch')
+let btnSearch = document.getElementById('btnSearch')
+let gamesPlace = document.getElementById('tela')
+
+function doSearch() {
+    let textoPesquisa = txtSearch.value
+    let url = `https://api.rawg.io/api/games?key=${apiKey}&search=${textoPesquisa}`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            let str = ''
+            for (let i = 0; i < data.results.length; i++) {
+                let jogo = data.results[i]
+                str += `<div class="card">
+                        <img src="${jogo.background_image}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"<b>${jogo.name}</b></h5>
+                            <p class="card-text">
+                            ${jogo.released}
+                            </p>
+                            <a href="https://rawg.io/games/${jogo.id}" target="_blank" class="btn btn-primary">Mais detalhes</a>
+                        </div>
+                        </div>`
+            }
+            document.getElementById('tela').innerHTML = str
+        })
+}
+
+document.body.onload = () => {
+    btnSearch.addEventListener('click', doSearch)
+}
+
